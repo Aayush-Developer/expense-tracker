c6011016b2093f1bd9ad2864d470cbb7cbe9f4ee
aayush <asaonew4393@gmail.com>
2026-07-05 02:34:32 +0530

    baseURL updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:4000"
+    baseURL: "https://expense-tracker-1-backend-j1mg.onrender.com"
 });
 
 export default api;
