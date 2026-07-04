248810b2e7dd8248a4d3b72b57dddf5ecfb271e0
aayush <asaonew4393@gmail.com>
2026-07-05 03:05:39 +0530

    changed cors

--- backend/index.js
+++ backend/index.js
@@ -14,15 +14,7 @@ const PORT = process.env.PORT || 4000;
 
 
 //middlewares
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://your-app-name.vercel.app"
-    ],
-    credentials: true
-  })
-);
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
